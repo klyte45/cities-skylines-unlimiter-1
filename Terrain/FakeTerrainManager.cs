70e583f3ba5d81447982b21bbfe3a7d286fdb38b
Egor Aralov <aei.omsk@gmail.com>
2015-12-11 01:31:28 +0600

    Clean up FakeTerrainManager

--- Terrain/FakeTerrainManager.cs
+++ Terrain/FakeTerrainManager.cs
@@ -1,58 +1,64 @@
-﻿using UnityEngine;
+﻿using EightyOne.Areas;
+using UnityEngine;
 using EightyOne.Attributes;
 
 namespace EightyOne.Terrain
 {
 
-    //TODO(earalov): review this class
     [TargetType(typeof(TerrainManager))]
-    public class FakeTerrainManager
+    public class FakeTerrainManager : TerrainManager
     {
         public static void Init()
         {
-        //    var tm = TerrainManager.instance;
-        //    tm.m_detailHeights = new ushort[231361 * 81];
-        //    tm.m_detailSurface = new TerrainManager.SurfaceCell[230400 * 81];
-        //    tm.m_detailZones = new TerrainManager.ZoneCell[230400 * 81];
+        }
+
+        //[ReplaceMethod]
+        public float GetUnlockableTerrainFlatness()
+        {
+            float num1 = 0.0f;
+            //begin mod
+            for (int index1 = 0; index1 < FakeGameAreaManager.GRID; ++index1)
+            {
+                for (int index2 = 0; index2 < FakeGameAreaManager.GRID; ++index2)
+                    num1 += this.GetTileFlatness(index2, index1);
+            }
+            return num1 / (FakeGameAreaManager.GRID * FakeGameAreaManager.GRID);
+            //end mod
         }
 
         [ReplaceMethod]
         public float GetTileFlatness(int x, int z)
         {
-            TerrainPatch terrainPatch = TerrainManager.instance.m_patches[z * 9 + x];
-            return terrainPatch.m_flatness;
+            //begin mod
+            return this.m_patches[z * 9 + x].m_flatness;
+            //end mod
         }
 
         [ReplaceMethod]
         public TerrainManager.SurfaceCell GetSurfaceCell(int x, int z)
         {
-            var tm = TerrainManager.instance;
-            int num = Mathf.Clamp(x / 480,0,8);
-            int num2 = Mathf.Clamp(z / 480,0, 8);
-            int num3 = num2 * 9 + num;
-            int simDetailIndex = tm.m_patches[num3].m_simDetailIndex;
-            if (simDetailIndex == 0)
-            {
-                return tm.SampleRawSurface((float)x * 0.25f, (float)z * 0.25f);
-            }
-            int num4 = (simDetailIndex - 1) * 480 * 480;
-            int num5 = x - num * 480;
+            //begin mod
+            int num1 = Mathf.Clamp(x / 480, 0, 8);
+            int num2 = Mathf.Clamp(z / 480, 0, 8);
+            //end mod
+            int index = num2 * 9 + num1;
+            int num3 = this.m_patches[index].m_simDetailIndex;
+            if (num3 == 0)
+                return this.SampleRawSurface((float)x * 0.25f, (float)z * 0.25f);
+            int num4 = (num3 - 1) * 480 * 480;
+            int num5 = x - num1 * 480;
             int num6 = z - num2 * 480;
-
-            var maxLimit = tm.m_detailSurface.Length - 1;
-            if ((num5 == 0 && num != 0 && tm.m_patches[num3 - 1].m_simDetailIndex == 0) || (num6 == 0 && num2 != 0 && tm.m_patches[num3 - 9].m_simDetailIndex == 0))
+            if (num5 == 0 && num1 != 0 && this.m_patches[index - 1].m_simDetailIndex == 0 || num6 == 0 && num2 != 0 && this.m_patches[index - 9].m_simDetailIndex == 0)
             {
-                TerrainManager.SurfaceCell result = tm.SampleRawSurface((float)x * 0.25f, (float)z * 0.25f);
-                result.m_clipped = tm.m_detailSurface[Mathf.Clamp(num4 + num6 * 480 + num5,0,maxLimit)].m_clipped;
-                return result;
+                TerrainManager.SurfaceCell surfaceCell = this.SampleRawSurface((float)x * 0.25f, (float)z * 0.25f);
+                surfaceCell.m_clipped = this.m_detailSurface[num4 + num6 * 480 + num5].m_clipped;
+                return surfaceCell;
             }
-            if ((num5 == 479 && num != 8 && tm.m_patches[num3 + 1].m_simDetailIndex == 0) || (num6 == 479 && num2 != 8 && tm.m_patches[num3 + 9].m_simDetailIndex == 0))
-            {
-                TerrainManager.SurfaceCell result2 = tm.SampleRawSurface((float)x * 0.25f, (float)z * 0.25f);
-                result2.m_clipped = tm.m_detailSurface[Mathf.Clamp(num4 + num6 * 480 + num5,0,maxLimit)].m_clipped;
-                return result2;
-            }
-            return tm.m_detailSurface[Mathf.Clamp(num4 + num6 * 480 + num5, 0, maxLimit)];
+            if ((num5 != 479 || num1 == 8 || this.m_patches[index + 1].m_simDetailIndex != 0) && (num6 != 479 || num2 == 8 || this.m_patches[index + 9].m_simDetailIndex != 0))
+                return this.m_detailSurface[num4 + num6 * 480 + num5];
+            TerrainManager.SurfaceCell surfaceCell1 = this.SampleRawSurface((float)x * 0.25f, (float)z * 0.25f);
+            surfaceCell1.m_clipped = this.m_detailSurface[num4 + num6 * 480 + num5].m_clipped;
+            return surfaceCell1;
         }
     }
 }
