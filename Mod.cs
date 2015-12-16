5e5ceb3afc47d4f8176030ac3f08f2d5ea6830d5
Egor Aralov <aei.omsk@gmail.com>
2015-12-16 17:28:02 +0600

    Clean up

--- Mod.cs
+++ Mod.cs
@@ -4,14 +4,8 @@ namespace EightyOne
 {
     public class Mod : IUserMod
     {
-        public string Description
-        {
-            get { return "Allows to use all 81 map tiles"; }
-        }
+        public string Description => "Allows to use all 81 map tiles";
 
-        public string Name
-        {
-            get { return "81 Tiles (Fixed for C:S 1.2+)"; }
-        }
+        public string Name => "81 Tiles (Fixed for C:S 1.2+)";
     }
 }
