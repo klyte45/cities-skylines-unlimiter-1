91afcc053c3b269390928508cee712c8ccfb99b8
Egor Aralov <aei.omsk@gmail.com>
2016-05-23 18:25:24 +0600

    Add surface painter compatibility

---
+++ Redirection/IgnoreIfSurfacePainterEnabledAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace EightyOne.Redirection
+{
+    public class IgnoreIfSurfacePainterEnabledAttribute : IgnoreIfOtherModEnabledAttribute
+    {
+        public IgnoreIfSurfacePainterEnabledAttribute() : base(Mod.SURFACE_PAINTER_MOD)
+        {
+        }
+    }
+}
