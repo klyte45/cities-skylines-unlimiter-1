d79a36a75c61112deee37e73a790ac70e18a32c2
earalov <earalov@thumbtack.net>
2016-11-30 16:24:49 +0600

    fix FakeBuilding

--- Zones/FakeBuilding.cs
+++ Zones/FakeBuilding.cs
@@ -1,4 +1,5 @@
-﻿using ColossalFramework;
+﻿using System.Runtime.CompilerServices;
+using ColossalFramework;
 using ColossalFramework.Math;
 using UnityEngine;
 using EightyOne.Redirection;
@@ -10,18 +11,15 @@ namespace EightyOne.Zones
     internal struct FakeBuilding
     {
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         [RedirectReverse(true)]
-        private static void CheckZoning(ref Building building, ItemClass.Zone zone1, ItemClass.Zone zone2, ref uint validCells, ref bool secondary, ref ZoneBlock block)
+        private static void CheckZoning(ref Building b, ItemClass.Zone zone1, ItemClass.Zone zone2, ref uint validCells, ref bool secondary, ref ZoneBlock block)
         {
-            UnityEngine.Debug.Log($"{building}-{zone1}-{zone2}-{validCells}-{secondary}-{block}");
-            UnityEngine.Debug.Log("AAAA");
-            UnityEngine.Debug.Log("BBBB");
-            UnityEngine.Debug.Log("CCCC");
-            UnityEngine.Debug.Log("DDDD");
+            UnityEngine.Debug.LogError("FakeBuilding - Failed to redirect reverse CheckZoning()");
         }
 
         [RedirectMethod]
-        public static bool CheckZoning(ref Building b, ItemClass.Zone zone1, ItemClass.Zone zone2)
+        public static bool CheckZoning(ref Building b, ItemClass.Zone zone1, ItemClass.Zone zone2, bool allowCollapsed)
         {
             int width = b.Width;
             int length = b.Length;
@@ -43,7 +41,7 @@ namespace EightyOne.Zones
             int num4 = Mathf.Min((int)(((double)vector3_6.z + 46.0) / 64.0 + FakeZoneManager.HALFGRID), FakeZoneManager.GRIDSIZE - 1);
             //end mod
             bool secondary = false;
-            uint validCells = 0U;
+            uint validCells = 0;
             ZoneManager instance = Singleton<ZoneManager>.instance;
             for (int index1 = num2; index1 <= num4; ++index1)
             {
@@ -55,15 +53,14 @@ namespace EightyOne.Zones
                     int num6 = 0;
                     while ((int)num5 != 0)
                     {
-                        Vector3 vector3_7 = instance.m_blocks.m_buffer[(int)num5].m_position;
-                        if ((double)Mathf.Max(Mathf.Max(vector3_5.x - 46f - vector3_7.x, vector3_5.z - 46f - vector3_7.z), Mathf.Max((float)((double)vector3_7.x - (double)vector3_6.x - 46.0), (float)((double)vector3_7.z - (double)vector3_6.z - 46.0))) < 0.0)
+                        if (allowCollapsed || ((int)instance.m_blocks.m_buffer[(int)num5].m_flags & 4) == 0)
                         {
-                            //begin mod
-                            CheckZoning(ref b, zone1, zone2, ref validCells, ref secondary, ref instance.m_blocks.m_buffer[(int)num5]);
-                            //end mod
+                            Vector3 vector3_7 = instance.m_blocks.m_buffer[(int)num5].m_position;
+                            if ((double)Mathf.Max(Mathf.Max(vector3_5.x - 46f - vector3_7.x, vector3_5.z - 46f - vector3_7.z), Mathf.Max((float)((double)vector3_7.x - (double)vector3_6.x - 46.0), (float)((double)vector3_7.z - (double)vector3_6.z - 46.0))) < 0.0)
+                                CheckZoning(ref b, zone1, zone2, ref validCells, ref secondary, ref instance.m_blocks.m_buffer[(int)num5]);
                         }
                         num5 = instance.m_blocks.m_buffer[(int)num5].m_nextGridBlock;
-                        if (++num6 >= ZoneManager.MAX_BLOCK_COUNT)
+                        if (++num6 >= 49152)
                         {
                             CODebugBase<LogChannel>.Error(LogChannel.Core, "Invalid list detected!\n" + System.Environment.StackTrace);
                             break;
@@ -76,9 +73,7 @@ namespace EightyOne.Zones
                 for (int index2 = 0; index2 < width; ++index2)
                 {
                     if (((int)validCells & 1 << (index1 << 3) + index2) == 0)
-                    {
                         return false;
-                    }
                 }
             }
             if (!secondary ? zone1 == ItemClass.Zone.CommercialHigh || zone1 == ItemClass.Zone.ResidentialHigh : zone2 == ItemClass.Zone.CommercialHigh || zone2 == ItemClass.Zone.ResidentialHigh)
