a36a9532bb459ad3a9bb4e67201c3a68e7abc5e1
Bloody Penguin <aei.omsk@gmail.com>
2019-05-28 23:57:08 +0200

    Fix not in campus area issue outside of 25 tiles. Add self-checks.

--- LoadingExtension.cs
+++ LoadingExtension.cs
@@ -1,7 +1,8 @@
-﻿using ColossalFramework;
+﻿using System;
+using ColossalFramework;
 using ColossalFramework.UI;
 using ICities;
-using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace EightyOne
 {
@@ -34,44 +35,86 @@ namespace EightyOne
                     $"'81 Tiles' isn't compatible with 'All Tile Start' mod!\nPlease unsubscribe that mod and use '{Mod.UNLOCK_ALL_TILES_FOR_FREE}' button in '81 Tiles' options entry!",
                 false);
             }
+            SimulationManager.instance.AddAction(FixNotInIndustryAreaProblem);
+            SimulationManager.instance.AddAction(FixNotInCampusAreaProblem);
 
-            SimulationManager.instance.AddAction(FixNotInParkProblem);
+            if (Enum.GetNames(typeof(DistrictPark.ParkType)).Length > 14)
+            {
+                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel").SetMessage("81 Tiles self-check: new park types detected", "This new version of the game added new park types. 81 Tiles has to be updated in order to not get 'Not in X area' errors on save re-load", false);
+            }
+            
+            if (Enum.GetNames(typeof(ImmaterialResourceManager.Resource)).Length > 28)
+            {
+                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel").SetMessage("81 Tiles self-check: new resource types detected", "This new version of the game added new resource types. 81 Tiles has to be updated!", false);
+            }
             
         }
 
-        private static void FixNotInParkProblem()
+        private static void FixNotInIndustryAreaProblem()
+        {
+            FixNotInParkProblem<IndustryBuildingAI>(Notification.Problem.NotInIndustryArea, GetIndustryPark);
+        }
+        
+        private static void FixNotInCampusAreaProblem()
+        {
+            FixNotInParkProblem<CampusBuildingAI>(Notification.Problem.NotInCampusArea, GetCampusPark);
+        }
+
+        private static void FixNotInParkProblem<T>(Notification.Problem problem, Func<byte, Building, byte> getParkFunction) where T : PlayerBuildingAI
         {
             for (uint i = 0; i < BuildingManager.instance.m_buildings.m_size; i++)
             {
                 var data = BuildingManager.instance.m_buildings.m_buffer[i];
-                if (data.Info == null || !(data.Info.m_buildingAI is IndustryBuildingAI))
+                if (data.Info == null || !(data.Info.m_buildingAI is T))
                 {
                     continue;
                 }
                 DistrictManager instance = Singleton<DistrictManager>.instance;
-                if ((data.m_problems & Notification.Problem.NotInIndustryArea) != Notification.Problem.None)
+                if ((data.m_problems & problem) != Notification.Problem.None)
                 {
                     byte park = instance.GetPark(data.m_position);
                     if (park != (byte) 0)
                     {
-                        if (!instance.m_parks.m_buffer[(int) park].IsIndustry)
-                            park = (byte) 0;
-                        else
-                        {
-                            var industryBuildingAi = ((IndustryBuildingAI) data.Info.m_buildingAI);
-                            if (industryBuildingAi.m_industryType == DistrictPark.ParkType.Industry ||
-                                industryBuildingAi.m_industryType != instance.m_parks.m_buffer[(int) park].m_parkType)
-                                park = (byte) 0;
-                        }
+                        park = getParkFunction(park, data);
                     }
 
                     if (park != 0)
                     {
                         BuildingManager.instance.m_buildings.m_buffer[i].m_problems = Notification.RemoveProblems(data.m_problems,
-                            Notification.Problem.NotInIndustryArea);
+                            problem);
                     }
                 }
             }
+        }
+
+        private static byte GetIndustryPark(byte park, Building data)
+        {
+            DistrictManager instance = Singleton<DistrictManager>.instance;
+            if (!instance.m_parks.m_buffer[(int) park].IsIndustry)
+                park = (byte) 0;
+            else
+            {
+                var industryBuildingAi = ((IndustryBuildingAI) data.Info.m_buildingAI);
+                if (industryBuildingAi.m_industryType == DistrictPark.ParkType.Industry ||
+                    industryBuildingAi.m_industryType != instance.m_parks.m_buffer[(int) park].m_parkType)
+                    park = (byte) 0;
+            }
+            return park;
+        }
+        
+        private static byte GetCampusPark(byte park, Building data)
+        {
+            DistrictManager instance = Singleton<DistrictManager>.instance;
+            if (!instance.m_parks.m_buffer[(int) park].IsCampus)
+                park = (byte) 0;
+            else
+            {
+                var campusBuildingAi = ((CampusBuildingAI) data.Info.m_buildingAI);
+                if (campusBuildingAi.m_campusType == DistrictPark.ParkType.GenericCampus ||
+                    campusBuildingAi.m_campusType != instance.m_parks.m_buffer[(int) park].m_parkType)
+                    park = (byte) 0;
+            }
+            return park;
         }
 
         public override void OnLevelUnloading()
